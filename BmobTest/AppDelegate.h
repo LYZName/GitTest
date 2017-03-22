8eca751bf4d614fe70e7209f75d599a73ecc4292
liyazhou <1286041054@qq.com>
2017-03-22 12:11:14 +0800

    Initial Commit

---
+++ BmobTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BmobTest
+//
+//  Created by liyazhou on 17/3/22.
+//  Copyright © 2017年 liyazhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
